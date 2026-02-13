cb0ac37c0fbc87634c1be8cb8107c693d6ad1d45
Daniel Alexandrov <169899785+danielalexandrov@users.noreply.github.com>
2026-02-13 13:03:25 +0100

    added matrix struct + rudimentary functions

--- main.c
+++ main.c
@@ -2,29 +2,37 @@
 #include <stdlib.h>
 
 typedef struct {
-  int length;
-  int *numbers;
-} custom_array;
+  int row_count;
+  int col_count;
+  double *contents;
+} matrix;
+
+matrix create_matrix(int rows, int cols) {
+  matrix m = {rows, cols, malloc(rows * cols * sizeof(double))};
+  return m;
+}
+
+double element_at_ij(matrix m, int i, int j) {
+  return m.contents[i * m.col_count + j];
+}
+
+void set_element_at_ij(matrix m, int i, int j, double element) {
+  m.contents[i * m.col_count + j] = element;
+}
 
 int main() {
-  printf("Hello World\n");
 
-  int test2[5];
+  matrix test = create_matrix(5, 6);
 
-  for (int i = 0; i < 5; i++) {
-    test2[i] = i;
-    printf("%d ", test2[i]);
+  for (int i = 0; i < test.row_count; i++) {
+    for (int j = 0; j < test.col_count; j++) {
+      set_element_at_ij(test, i, j, 0);
+    }
   }
 
-  printf("\n");
-  custom_array test;
-  test.length = 5;
-  test.numbers = malloc(test.length * sizeof(int));
-  for (int i = 0; i < test.length; i++) {
-    test.numbers[i] = i;
-    printf("%d ", test.numbers[i]);
-  }
+  set_element_at_ij(test, 2, 3, 22);
 
-  printf("\n");
-  free(test.numbers);
+  double test_element = element_at_ij(test, 2, 3);
+
+  printf("%.2f\n", test_element);
 }
