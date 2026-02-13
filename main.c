c686fa24be999cfa419dd303aafc504106f1040f
Daniel Alexandrov <169899785+danielalexandrov@users.noreply.github.com>
2026-02-13 19:32:01 +0100

    added sum func and determinant func for 2x2 matrices

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ matrix create_matrix(int rows, int cols) {
   return m;
 }
 
+void delete_matrix(matrix *m) { free(m->contents); }
+
 double element_at_ij(matrix *m, int i, int j) {
   return m->contents[(i - 1) * m->col_count +
                      (j - 1)]; // subtract 1 from each index to ensure indeces
@@ -26,19 +28,22 @@ void set_element_at_ij(matrix *m, int i, int j, double element) {
 
 matrix sum_matrices(matrix *m, matrix *n) {
   matrix result = create_matrix(m->row_count, m->col_count);
-  if (m->row_count != n->row_count && m->col_count != n->row_count) {
-    perror("Cannot add matrices due to unequal dimensions");
+  if (m->row_count != n->row_count && m->col_count != n->col_count) {
+    printf("Cannot add matrices due to unequal dimensions\n");
     return result;
   }
 
   for (int i = 0; i < m->row_count; i++) {
     for (int j = 0; j < m->col_count; j++) {
-      double sum = element_at_ij(m, i, j) + element_at_ij(n, i, j);
-      set_element_at_ij(&result, i, j, sum);
+      double sum =
+          element_at_ij(m, i + 1, j + 1) + element_at_ij(n, i + 1, j + 1);
+      set_element_at_ij(&result, i + 1, j + 1, sum);
     }
   }
   return result;
 }
+
+// TODO add subtract_matrices
 
 matrix multiply_matrices(matrix *m, matrix *n) {
   matrix result = create_matrix(m->row_count, n->col_count);
@@ -62,10 +67,26 @@ matrix multiply_matrices(matrix *m, matrix *n) {
   return result;
 }
 
+double determinant(matrix *m) {
+  double result = 0;
+  if (m->row_count != m->col_count) {
+    perror("This matrix is not square. Determinant cannot be computed.");
+    return 0;
+  }
+
+  if (m->row_count == 2) {
+    result = element_at_ij(m, 1, 1) * element_at_ij(m, 2, 2) -
+             element_at_ij(m, 1, 2) * element_at_ij(m, 2, 1);
+  }
+  // TODO add determinant calculation for 3x3 and higher order matrices
+  return result;
+}
+
 int main() {
 
   matrix test = create_matrix(2, 3);
   matrix test2 = create_matrix(3, 2);
+  matrix test3 = create_matrix(3, 2);
 
   for (int i = 0; i < test.row_count; i++) {
     for (int j = 0; j < test.col_count; j++) {
@@ -80,8 +101,12 @@ int main() {
   }
 
   matrix result = multiply_matrices(&test, &test2);
+  result = sum_matrices(&test2, &test3);
 
   double test_element = element_at_ij(&result, 2, 2);
 
   printf("%.2f\n", test_element);
+
+  delete_matrix(&test);
+  delete_matrix(&test2);
 }
