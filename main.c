23c23f7d475cc9801c2dd7a2dde2e5336b11595f
Daniel Alexandrov <169899785+danielalexandrov@users.noreply.github.com>
2026-02-13 04:43:46 +0100

    added custom array struct

--- main.c
+++ main.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() { printf("Hello World\n"); }
+typedef struct {
+  int length;
+  int *numbers;
+} custom_array;
+
+int main() {
+  printf("Hello World\n");
+
+  int test2[5];
+
+  for (int i = 0; i < 5; i++) {
+    test2[i] = i;
+    printf("%d ", test2[i]);
+  }
+
+  printf("\n");
+  custom_array test;
+  test.length = 5;
+  test.numbers = malloc(test.length * sizeof(int));
+  for (int i = 0; i < test.length; i++) {
+    test.numbers[i] = i;
+    printf("%d ", test.numbers[i]);
+  }
+
+  printf("\n");
+  free(test.numbers);
+}
